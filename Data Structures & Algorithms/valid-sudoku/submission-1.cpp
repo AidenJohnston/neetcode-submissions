d61353fb59ee05180548551a117a244f3cbeab78
Aiden Johnston <aidendj@icloud.com>
2026-05-15 10:17:11 -0500

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,86 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        //check each row
+        for (int x = 0; x < 9; x++) {
+            vector<char> vec = board[x];
+            unordered_set<char> seen;
+            for (const char& letter : vec) {
+                if (!seen.insert(letter).second  && (letter != '.')) {
+                    return false;
+                }
+            }
+        }
+
+        //check each column
+        for (int x = 0; x < 9; x++) {
+            vector<char> vec;
+            for (int y = 0; y < 9; y++) {
+                vec.push_back(board.at(y).at(x));
+            }
+            unordered_set<char> seen;
+            for (const char& letter : vec) {
+                if (!seen.insert(letter).second  && (letter != '.')) {
+                    return false;
+                }
+            }
+        }
+
+        //check each box
+        for (int x = 0; x < 9; x++) {
+            int xOffset, yOffset;
+            switch(x) {
+                case 0:
+                    xOffset = 0;
+                    yOffset = 0;
+                    break;
+                case 1:
+                    xOffset = 3;
+                    yOffset = 0;
+                    break;
+                case 2:
+                    xOffset = 6;
+                    yOffset = 0;
+                    break;
+                case 3:
+                    xOffset = 0;
+                    yOffset = 3;
+                    break;
+                case 4:
+                    xOffset = 3;
+                    yOffset = 3;
+                    break;
+                case 5:
+                    xOffset = 6;
+                    yOffset = 3;
+                    break;
+                case 6:
+                    xOffset = 0;
+                    yOffset = 6;
+                    break;
+                case 7:
+                    xOffset = 3;
+                    yOffset = 6;
+                    break;
+                case 8:
+                    xOffset = 6;
+                    yOffset = 6;
+                    break;
+            }
+            vector<char> vec;
+            for (int y = 0; y < 3; y++) {
+                for (int z = 0; z < 3; z++) {
+                    vec.push_back(board.at(y+xOffset).at(z+yOffset));
+                }
+            }
+            unordered_set<char> seen;
+            for (const char& letter : vec) {
+                if (!seen.insert(letter).second  && (letter != '.')) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
