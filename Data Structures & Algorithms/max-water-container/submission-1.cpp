f77de09f8ea26f419bd1b1bc3ff576728e6d45bc
Aiden Johnston <aidendj@icloud.com>
2026-05-20 11:35:05 -0500

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int left {0};
+        int right {static_cast<int>(heights.size()-1)};
+
+        int maxWater {0};
+
+        while (left < right) {
+            int currentWater {min(heights[left], heights[right]) * (right-left)};
+
+            maxWater = max(maxWater, currentWater);
+
+            if (heights[left] < heights[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+};
