0de01789bf26b7363e8bc940fb801c339152ad65
Aiden Johnston <aidendj@icloud.com>
2026-06-06 13:07:17 +0200

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows {static_cast<int>(matrix.size())};
+        int cols {static_cast<int>(matrix[0].size())};
+
+        int low {0};
+        int high {rows-1};
+        int targetRow {-1};
+        while (low <= high) {
+            int mid {low + (high - low) / 2};
+
+            if (target >= matrix[mid][0] && target <= matrix[mid][cols-1]) {
+                targetRow = mid;
+                break;
+            }
+            else if (target < matrix[mid][0]) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        if (targetRow == -1) {
+            return false;
+        }
+
+        low = 0;
+        high = cols - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (matrix[targetRow][mid] == target) {
+                return true;
+            }
+            else if (matrix[targetRow][mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
